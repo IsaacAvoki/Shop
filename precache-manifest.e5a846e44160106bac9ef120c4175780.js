ccedb74c248f5073a96f2013245c5cd889100023
unknown <ipaul9521@gmail.com>
2023-03-04 09:35:26 +0200

    Updates

---
+++ precache-manifest.e5a846e44160106bac9ef120c4175780.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eb4890e11acf209a54e0232887a9d12",
+    "url": "/Shop/index.html"
+  },
+  {
+    "revision": "9be0dc4df1c89b97aafa",
+    "url": "/Shop/static/css/main.eafeacb5.chunk.css"
+  },
+  {
+    "revision": "8f0aac18e45bf5525a34",
+    "url": "/Shop/static/js/2.6ed1760b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Shop/static/js/2.6ed1760b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9be0dc4df1c89b97aafa",
+    "url": "/Shop/static/js/main.6bdf67b5.chunk.js"
+  },
+  {
+    "revision": "92f02704f7a3dbb1aed6",
+    "url": "/Shop/static/js/runtime-main.e7172133.js"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/Shop/static/media/bars-solid.b3f49afd.svg"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/Shop/static/media/shopping-cart-solid.8bf1af37.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/Shop/static/media/times-solid.2c5552a1.svg"
+  }
+]);
