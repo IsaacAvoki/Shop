515ece54b7a8ff405e661e0dbfae57221462ae8d
unknown <ipaul9521@gmail.com>
2023-09-09 07:42:48 -0400

    Final but to update

--- src/components/Header.js
+++ src/components/Header.js
@@ -29,15 +29,13 @@ export class Header extends Component {
                     <img src={Menu} alt="" width="20"/>
                 </div>
                 <div className="logo">
-                    <h1><Link to="/">Nike</Link></h1>
+                    <h1><Link to="/">DNM STORE</Link></h1>
                 </div>
                 <nav>
                     <ul className={toggle ? "toggle" : ""}>
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="/product">Product</Link></li>
-                        <li><Link to="/contact">Contact</Link></li>
-                        <li><Link to="/about">About</Link></li>
-                        <li><Link to="/login">Login / Register</Link></li>
+                       
                         <li className="close" onClick={this.menuToggle}>
                             <img src={Close} alt="" width="20"/>
                         </li>
--- src/components/section/Products.js
+++ src/components/section/Products.js
@@ -1,36 +1,48 @@
-import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
-import {DataContext} from '../Context'
-import '../css/Products.css'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { DataContext } from '../Context';
+import '../css/Products.css';
 
 export class Products extends Component {
-
     static contextType = DataContext;
 
     render() {
-        const {products,addCart} = this.context;
+        const { products, addCart } = this.context;
+
+        // Define the URL of the proteine image
+        const proteineImageURL = 'https://images.unsplash.com/photo-1579722822280-a3d601518cc9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjJ8fHByb3RlaW4lMjBwb3dkZXJ8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60';
+
         return (
             <div id="product">
-               {
-                   products.map(product =>(
-                       <div className="card" key={product._id}>
-                           <Link to={`/product/${product._id}`}>
-                               <img src={product.src} alt=""/>
-                           </Link>
-                           <div className="content">
-                               <h3>
-                                   <Link to={`/product/${product._id}`}>{product.title}</Link>
-                               </h3>
-                               <span>${product.price}</span>
-                               <p>{product.description}</p>
-                               <button onClick={()=> addCart(product._id)}>Add to cart</button>
-                           </div>
-                       </div>
-                   ))
-               }
+                <section
+                    className="proteine-section"
+                    style={{
+                        backgroundImage: `url(${proteineImageURL})`,
+                        backgroundRepeat: 'no-repeat',
+                    }}
+                >
+                    <h2>Ventes des chaussures, protéines et accessoires de sport</h2>
+                </section>
+                {
+                    products.map(product => (
+                        <div className="card" key={product._id}>
+                            <Link to={`/product/${product._id}`}>
+                                <img src={product.src} alt="" />
+                            </Link>
+                            <div className="content">
+                                <h3>
+                                    <Link to={`/product/${product._id}`}>{product.title}</Link>
+                                </h3>
+                                <span>${product.price}</span>
+                                <p>{product.description}</p>
+                                <button onClick={() => addCart(product._id)}>Détails du produit</button>
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
-        )
+        );
     }
 }
 
-export default Products
+export default Products;
